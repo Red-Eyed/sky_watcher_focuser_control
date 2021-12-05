5d54aa79f6da7569e1200803d623f850097228d8
Vadym Stupakov <vadim.stupakov@gmail.com>
2021-12-06 01:19:23 +0200

    added several baudrates to finder

--- python/controller.py
+++ python/controller.py
@@ -16,43 +16,52 @@ logger = logging.getLogger(__name__)
 
 
 class FocuserController:
+    _BAUDRATES = (300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 115200)
+
+    def __init__(self):
+        self._found = False
+
     @cached_property
     def ports(self):
         ports = list_ports.comports()
         logger.info(f"PORTS: {[p.description for p in ports]}")
         return ports[::-1]
 
-    @staticmethod
-    def _find_focuser_port_worker(port, baudrate):
+    def _find_focuser_port_worker(self, port, baudrates):
         logger.debug(f"Trying port: {port.device}")
         handshake_req = "who are you?"
         handshake_resp = "focuser"
 
-        # Avoid arduino board from resetting on Serial connection
-        ser = serial.Serial()
-        ser.baudrate = baudrate
-        ser.port = port.device
-        ser.timeout = 2
-        ser.write_timeout = 2
-        ser.setDTR(False)
-        ser.open()
+        for baudrate in baudrates:
+            if self._found:
+                return
 
-        ser.write(handshake_req.encode("utf8"))
-        res = ser.readline().decode().strip()
+            # Avoid arduino board from resetting on Serial connection
+            ser = serial.Serial()
+            ser.baudrate = baudrate
+            ser.port = port.device
+            ser.timeout = 2
+            ser.write_timeout = 2
+            ser.setDTR(False)
+            ser.open()
 
-        if res == handshake_resp:
-            cfg = ser.get_settings()
-            cfg = {port.device: cfg}
-            logger.info(f"Found port: {pformat(cfg)}")
-            return ser
-        else:
-            ser.close()
+            ser.write(handshake_req.encode("utf8"))
+            res = ser.readline().decode().strip()
+
+            if res == handshake_resp:
+                cfg = ser.get_settings()
+                cfg = {port.device: cfg}
+                logger.info(f"Found port: {pformat(cfg)}")
+                self._found = True
+                return ser
+            else:
+                ser.close()
 
     def find_focuser_port(self):
         with ThreadPoolExecutor() as e:
             futures = []
             for port in self.ports:
-                f = e.submit(self._find_focuser_port_worker, port, 9600)
+                f = e.submit(self._find_focuser_port_worker, port, self._BAUDRATES)
                 futures.append(f)
 
             for f in as_completed(futures):
